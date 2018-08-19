dc19271357b4cca3579588eead685094d2601c5b
DiegoVz74 <diego_val2008@hotmail.com>
2018-08-19 06:11:19 -0500

    nada

--- app/src/main/java/com/diego/ovocacional/fragments/ListaCarrerasFragment.java
+++ app/src/main/java/com/diego/ovocacional/fragments/ListaCarrerasFragment.java
@@ -23,8 +23,12 @@ public class ListaCarrerasFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lista_carreras, container, false);
+       View v = inflater.inflate(R.layout.fragment_lista_carreras, container, false);
+
+
+
+
+       return v;
     }
 
 
